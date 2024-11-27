4efa9ab83f9128be58e2cc67eca68e5f120c5bcc
silancetin0 <silancetin8@gmail.com>
2024-11-27 11:53:33 +0300

    Android_Location_Tracker

---
+++ UrlShortener/app/src/main/java/com/deneme/urlshortener/ApiServiceInterface.java
@@ -0,0 +1,11 @@
+package com.deneme.urlshortener;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface ApiServiceInterface {
+
+    @POST("shorten")
+    Call<ShortenResponse> shortenUrl(@Body ShortenRequest request);
+}
